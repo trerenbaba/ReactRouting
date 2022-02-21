376f05abd8d989d47cdcd27bb7abcf9e2f36bd1d
mert.alptekin <mert.alptekin@neominal.com>
2022-02-21 10:22:48 +0300

    Pages

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
+import { ListGroup, Modal } from 'react-bootstrap';
+import _ from 'lodash';
 
 function Users() {
 	const [first, setfirst] = useState('ali');
 	const [users, setUsers] = useState([]);
+	const [selectedUser, setSelectedUser] = useState(null); // modal kapalı
 
 	// useEffect ise component doma basıldığında tetiklenen bir hook
 	// apiden veri çekmek için idealdir. 1 kereye mahsus çalışır.
@@ -12,7 +15,8 @@ function Users() {
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((response) => response.json())
 			.then((data) => {
-				setUsers([...data]);
+				let orderedResponse = _.orderBy(data, ['username'], ['asc']);
+				setUsers([...orderedResponse]);
 			});
 	}, []); // [] şeklinde kullandığımızda ise component doma yüklenirken 1 kez tetiklenir.
 
@@ -35,19 +39,48 @@ function Users() {
 		setfirst('muhammed');
 	};
 
+	const hide = () => {
+		setSelectedUser(null);
+	};
+
+	// user seçim işlemi yaptık
+	const select = (item) => {
+		setSelectedUser(item);
+	};
+
 	return (
 		<div>
 			{/* {first}
 			<button onClick={changeName}>Change Name</button> */}
-			<ul>
+			<ListGroup>
 				{users.map((item) => {
 					return (
-						<li key={item.id}>
-							{item.username} {item.email}
-						</li>
+						<ListGroup.Item onClick={() => select(item)} key={item.id}>
+							{item.username}
+						</ListGroup.Item>
 					);
 				})}
-			</ul>
+			</ListGroup>
+
+			<Modal
+				size="sm"
+				show={selectedUser == null ? false : true}
+				onHide={() => hide()}
+				aria-labelledby="example-modal-sizes-title-sm"
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id="example-modal-sizes-title-sm">User Info</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<p>
+						<b>Email:</b> {selectedUser?.email}
+					</p>
+					<p>
+						<b>Address:</b> {selectedUser?.address?.street},{' '}
+						{selectedUser?.address?.suite} ,{selectedUser?.address?.city}
+					</p>
+				</Modal.Body>
+			</Modal>
 		</div>
 	);
 }
